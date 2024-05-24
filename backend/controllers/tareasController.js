081ca9cbba8c0e70440744a2bbbd69c537831b15
fersh00 <fc323897@gmail.com>
2024-05-23 18:45:54 -0700

    Actualizado, segunda clase

--- backend/controllers/tareasController.js
+++ backend/controllers/tareasController.js
@@ -1,37 +1,56 @@
 const asyncHandler = require('express-async-handler')
+const Tarea = require('../models/tareaModel')
 
-const getTareas = asyncHandler( async (req, res) => {
-    res.status(200).json({
-		msg: 'Get Tareas',
-	})
+const getTareas = asyncHandler(async (req, res) => {
+	const tareas = await Tarea.find()
+
+	res.status(200).json(tareas)
 })
 
-const crearTareas = asyncHandler(  async (req, res) => {
-    if(!req.body.texto) {
-        res.status(400)
-        throw new Error ("No teclaste el texto")        
-    }    
-    res.status(200).json({
-		msg: 'Crear Tareas',
+const crearTareas = asyncHandler(async (req, res) => {
+	if (!req.body.texto) {
+		res.status(400)
+		throw new Error('No teclaste el texto')
+	}
+	const tarea = await Tarea.create({
+		texto: req.body.texto,
 	})
+	res.status(201).json(tarea)
 })
 
-const updateTareas = asyncHandler( async (req, res) => {
-    res.status(201).json({
-		msg: `Actualizar tarea con id: ${req.params.id}`,
+const updateTareas = asyncHandler(async (req, res) => {
+	const tarea = await Tarea.findById(req.params.id)
+
+	if (!tarea) {
+		res.status(404)
+		throw new Error('La tarea solicitada no existe')
+	}
+
+	const tareaUpdated = await Tarea.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
 	})
+
+	res.status(200).json(tareaUpdated)
 })
 
-const deleteTareas = asyncHandler( async (req, res) => {
-    res.status(200).json({
-		msg: `Eliminar tarea con id: ${req.params.id}`,
-	})
+const deleteTareas = asyncHandler(async (req, res) => {
+
+	const tarea = await Tarea.findById(req.params.id)
+
+	if (!tarea) {
+		res.status(404)
+		throw new Error('La tarea solicitada no existe')
+	}
+
+	// const tareaDeleted = await Tarea.findByIdAndDelete(req.params.id)
+	await tarea.deleteOne()
+
+	res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
-    getTareas,
-    crearTareas,
-    updateTareas,
-    deleteTareas
+	getTareas,
+	crearTareas,
+	updateTareas,
+	deleteTareas,
 }
-
